6188d5571bdf69a5a8f13badd7534230ebe69534
andreas <andresun@stud.ntnu.no>
2022-02-16 13:05:01 +0100

    Forsøkte å implementere camera_opencv og run_client_opencv

--- scripts/run_client_opencv.py
+++ scripts/run_client_opencv.py
@@ -11,12 +11,12 @@ def main():
         client.sendall(b'Hello World!')
 
         # Tip: len(img.tostring())
-        size, data = client.recv(INSERT THE CORRECT IMAGE SIZE HERE)
+        size, data = client.recv(len(img.tostring()))
         if not data:
             break
 
         # Tip: img.dtype, img.shape
-        img = np.frombuffer(data, INSERT THE CORRECT DATA TYPE HERE).reshape(INSERT THE CORRECT IMAGE SHAPE HERE)
+        img = np.frombuffer(data, img.dtype).reshape(img.shape)
 
         cv2.imshow('img', img)
         if cv2.waitKey(20) == 27:  # Esc: 27
--- tpk4128/camera_opencv.py
+++ tpk4128/camera_opencv.py
@@ -5,16 +5,20 @@ import cv2 as cv
 class Camera(object):
 
     def __init__(self):
-
         # Implement this constructor that opens a webcam and stores it in self._camera
-        self._camera =
+        self._camera = cv.VideoCapture(0)
 
     def capture(self):
-
         # Implement this function that grabs an image from the webcam and returns a numpy array
+        ret, img = self._camera.read()
+
+        if not ret:
+            print("Can't receive frame (stream end?). Exiting ...")
+            return
 
         return img
 
     def __del__(self):
-
         # Implement this destructor. Remember to free the camera.
+        self._camera.release()
+        cv.destroyAllWindows()
