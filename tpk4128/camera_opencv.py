92230173d43502ed5d0936861f7e6a1fa8c82b0b
andreas <andresun@stud.ntnu.no>
2022-02-16 17:53:02 +0100

    Flyttet sleep i camera_opencv så den ikke forårsaker så mye lag.

--- tpk4128/camera_opencv.py
+++ tpk4128/camera_opencv.py
@@ -8,13 +8,14 @@ class Camera(object):
     def __init__(self):
         # Implement this constructor that opens a webcam and stores it in self._camera
         self._camera = cv.VideoCapture(0)
+        time.sleep(1.00)
 
     def capture(self):
         # Implement this function that grabs an image from the webcam and returns a numpy array
         ret, img = self._camera.read()
         print("Shape i camera_opencv")
         print(img.shape)
-        time.sleep(1.00) # Legges til for å gi windows tid til å finne kameraet. Fikset en error hvor den ikke klarte å acesse kameratet.
+         # Legges til for å gi windows tid til å finne kameraet. Fikset en error hvor den ikke klarte å acesse kameratet.
 
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
